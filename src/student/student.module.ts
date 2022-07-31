d14184dc99e7893b696c328e9d1f43b57acb3fc4
JakubL <jakubxleszczewski@gmail.com>
2022-07-31 19:49:21 +0200

    add endpoint get user by id

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
 import { UserModule } from '../user/user.module';
 import { StudentHelperService } from './student-helper.service';
 import { MailModule } from '../mail/mail.module';
+import { HrModule } from '../hr/hr.module';
 
 @Module({
-  imports: [UserModule, MailModule],
+  imports: [MailModule, forwardRef(() => UserModule)],
   controllers: [StudentController],
   providers: [StudentService, StudentHelperService],
   exports: [StudentService, StudentHelperService],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { UserRole } from '../types';
+import { UserHelperService } from './user-helper.service';
+import { StudentHelperService } from '../student/student-helper.service';
+import { HrHelperService } from '../hr/hr-helper.service';
 
 @Injectable()
 export class UserService {
+  constructor(
+    private userHelperService: UserHelperService,
+    private studentHelperService: StudentHelperService,
+    private hrHelperService: HrHelperService,
+  ) {}
+
   findAll() {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    if (!id) throw new BadRequestException();
+
+    const user = await User.findOne({
+      where: { id },
+      relations: ['student', 'hr'],
+    });
+    if (!user) throw new NotFoundException();
+
+    switch (user.role) {
+      case UserRole.Admin: {
+        return this.userHelperService.filterOnlyUser(user);
+      }
+      case UserRole.Student: {
+        return this.studentHelperService.filterStudent(user);
+      }
+      case UserRole.Hr: {
+        return this.hrHelperService.filterHr(user);
+      }
+      default: {
+        throw new Error('user role is empty');
+      }
+    }
   }
 
   update(id: number, updateUserDto: any) {
