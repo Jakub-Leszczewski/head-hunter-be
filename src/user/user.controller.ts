2a6a9641e7f2a6465ff9e8d8ee0bae71e5698ff4
JakubL <jakubxleszczewski@gmail.com>
2022-08-01 01:20:16 +0200

    protect findOne user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,22 @@
-import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Delete, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { FindUserResponse } from '../types/user/user-response';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { UserOwnerOrRoleGuard } from '../guards/user-owner-or-role.guard';
+import { SetRole } from '../decorators/set-role';
 
 @Controller('/api/user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard, UserOwnerOrRoleGuard)
+  @SetRole('admin', 'hr')
   async findOne(@Param('id') id: string): Promise<FindUserResponse> {
     return this.userService.findOne(id);
   }
