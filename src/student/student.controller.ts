a895094f000c745b0282653d8d814a223aae929d
JakubL <jakubxleszczewski@gmail.com>
2022-08-03 18:21:38 +0200

    change endpoints structure

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,34 +1,37 @@
-import { Controller, Post, Body, Patch, Param, UsePipes, UseGuards, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Patch,
+  Param,
+  UsePipes,
+  UseGuards,
+  Get,
+  Query,
+} from '@nestjs/common';
 import { StudentService } from './student.service';
 import { ImportStudentDto } from './dto/import-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
 import { ArrayValidationPipe } from '../common/pipes/ArrayValidationPipe';
-import { CreateStudentsResponse, GetStudentResponse, GetStudentsResponse } from '../types';
+import { CreateStudentsResponse, GetStudentsResponse } from '../types';
 import { CompletionStudentDto } from './dto/completion-student.dto';
 import { SetRole } from '../common/decorators/set-role';
 import { UserOwnerOrRoleGuard } from '../common/guards/user-owner-or-role.guard';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 import { RoleGuard } from '../common/guards/role.guard';
 
-@Controller('/api/user')
+@Controller('/api/student')
 export class StudentController {
-  constructor(private readonly studentService: StudentService) {}
+  constructor(private studentService: StudentService) {}
 
-  @Get('/student')
+  @Get('/')
   @UseGuards(JwtAuthGuard, RoleGuard)
   @SetRole('admin', 'hr')
   async findAll(): Promise<GetStudentsResponse> {
+    console.log('wow');
     return this.studentService.findAll();
   }
 
-  @Get('/:id/student')
-  @UseGuards(JwtAuthGuard, UserOwnerOrRoleGuard)
-  @SetRole('admin', 'hr')
-  async findOne(@Param('id') id: string): Promise<GetStudentResponse> {
-    return this.studentService.findOne(id);
-  }
-
-  @Post('/student')
+  @Post('/')
   @SetRole('admin')
   @UseGuards(JwtAuthGuard, RoleGuard)
   @UsePipes(ArrayValidationPipe(ImportStudentDto))
@@ -36,19 +39,12 @@ export class StudentController {
     return this.studentService.importStudents(createUserDto);
   }
 
-  @Patch('/student/:userToken')
+  @Patch('/:userToken')
   @UseGuards(JwtAuthGuard, UserOwnerOrRoleGuard)
   completeSignup(
     @Param('userToken') userToken: string,
     @Body() updateUserDto: CompletionStudentDto,
   ) {
     return this.studentService.completeSignup(userToken, updateUserDto);
-  }
-
-  @Patch('/:id/student')
-  @SetRole('admin')
-  @UseGuards(JwtAuthGuard, UserOwnerOrRoleGuard)
-  update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
-    return this.studentService.update(id, updateStudentDto);
   }
 }
